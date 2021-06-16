81e6d223355b936ec18b86607ba17c164f97c4ee
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-06-16 09:29:03 -0700

    Add files via upload

--- Math Master calculator.py
+++ Math Master calculator.py
@@ -1,5 +1,18 @@
 #Math Master Calculator
-import math
+import math, shelve
+def shelvewrite(textfilename,towrite): #write to text file
+    textfilenametxt = textfilename + '.txt'
+    writer = shelve.open(str(textfilenametxt))
+    writer[str(textfilename)] = towrite
+    writer.close()
+def shelveread(textfilename): # read form text file
+    textfilenametxt = textfilename + '.txt'
+    reader = shelve.open(str(textfilenametxt))
+    text = reader[str(textfilename)]
+    reader.close()
+    return text
+def clearscreen(): 
+    print('\n' * 50)
 def num_extractor(toread,numtoscan):
     if True:
         fish = toread
@@ -67,10 +80,12 @@ def num_extractor(toread,numtoscan):
     else:
         return  first, second, third, fourth
 def logs():
-    global command
+    global command, memory
     first, second = num_extractor(command,2)
     logged = math.log(second,first)        
     print('log ',first,second,'  = ',logged)
+    memory = memory + '\n' + 'log' + str(first) + str(second) + ' =' + str(logged)
+    shelvewrite('mastermathmemory',memory)
 def quadratic_formula():
     global command
     val1, val2, val3 = 0, 0, 0
@@ -97,14 +112,21 @@ def quadratic_formula():
             roundsqrtval = round(sqrtval,5)
             if val1 == 0:
                 print('+',roundsqrtval,'i   ,     -',roundsqrtval,'i')
+                memory = memory + '\n' + 'You entered: a =' + str(a)+'  b =' +str(b)+'  c =' + str(c) + '\nThe equation detected is' + str(equation) + '\nThe solution(s) are\n' + '+'+str(roundsqrtval)+'i   ,     -'+str(roundsqrtval)+'i'
+                shelvewrite('mastermathmemory',memory)
             else:
                 print(roundval1 , '+',roundsqrtval, 'i  ,  ', roundval1 , '-',roundsqrtval, 'i ')
+                memory = memory + '\n' + 'You entered: a =' + str(a)+'  b =' +str(b)+'  c =' + str(c) + '\nThe equation detected is' + str(equation) + '\nThe solution(s) are\n' + str(roundval1) + '+'+str(roundsqrtval)+ 'i  ,  '+ str(roundval1) + '-'+str(roundsqrtval)+ 'i '
+                shelvewrite('mastermathmemory',memory)
         else:
             val1 = val1 + math.sqrt(val3)
             val2 = val2 - math.sqrt(val3)
             val1 = (val1 / (2*a))
             val2 = (val2 / (2*a))
-            print(val1 if val1 == val2 else val1, val2)
+            toprint = val1 if val1 == val2 else val1, val2
+            print(toprint)
+            memory = memory + '\n' + 'You entered: a =' + str(a)+'  b =' +str(b)+'  c =' + str(c) + '\nThe equation detected is' + str(equation) + '\nThe solution(s) are' + str(toprint)
+            shelvewrite('mastermathmemory',memory)
 def factorfinder(number):
     number = int(number)
     factortry = 1
@@ -117,6 +139,11 @@ def factorfinder(number):
                 factors = list(str(factortry))
         factortry += 1
     return (factors)
+# initialize memory:
+try:
+    memory = shelveread('mastermathmemory')
+except:
+    shelvewrite('mastermathmemory','')
 while True:
     try:
         print('\n')
@@ -127,17 +154,19 @@ while True:
         print('Quadratic formula..............qua 1 11 30 for x²+11x+30')
         #print('Polynomial factorizer..........pol 1 -3 24 -31 for x³-x²+x-31')
         print('Number factorizer..............fac 24 for factorizing 24')
-        #print('Add list.......................add [list of numbers] ')
-        #print('Average of list................ave [list of numbers] ')
-        #print('Max value of list..............max [list of numbers] ')
-        #print('Min value of list..............min [list of numbers] ')
-        #print('Define a list..................def [list of numbers] ')
+        print('Trig ratios....................sec 23 for secant 23 degrees')
+        print('Convert deg to rad.............rad 23 to convert 23 degrees to radians')
+        #print('Add list.......................add list of numbers ')
+        #print('Average of list................ave list of numbers ')
+        #print('Max value of list..............max list of numbers ')
+        #print('Min value of list..............min list of numbers ')
+        #print('Define a list..................def list of numbers ')
         #print('Most recent answer.............ans')
         #print('To implement a list............(def) ex. max(def)')
         print('Mathematical calculation.......use regular math symbols ex. 34+52 56/4 7*7 2^5')
         print('Square root....................sqt 9 for square root 9')
         print('factorial......................fct')
-        #print('Show memory....................mem')
+        print('Show memory....................mem')
         print('Clear screen...................clr')
         print('Help center....................hlp')
         command = input('Enter command > ')
@@ -154,14 +183,23 @@ while True:
         if line == 'qua ':
             quadratic_formula()
         if line == 'fac ':
-            print('The factors of the number are: ', factorfinder(command))
+            facs = factorfinder(command)
+            print('The factors of the number are: ', facs)
+            memory = memory + '\n' + 'Factors of '+ str(command) + ' are ' + str(facs)
+            shelvewrite('mastermathmemory',memory)
         if line == 'fct ':
+            command = command.replace(' ','')
             command = int(command)
             print(math.factorial(command))
+            command = str(command)
+            memory = memory + '\n' + str(command) + ' factorial is ' + str(math.factorial(command))
+            shelvewrite('mastermathmemory',memory)
         if '+' in command:
             store = command.replace('+',' ')
             add1, add2 = num_extractor(store,2)
-            print(command,' = ', add1+add2)
+            toprint = command+' = '+ (add1+add2)
+            memory = memory + '\n' + toprint
+            shelvewrite('mastermathmemory',memory)
         if '-' in command:
             store = command.replace('-',' ')
             add1, add2 = num_extractor(store,2)
@@ -181,6 +219,27 @@ while True:
         if line == 'sqt ':
             command = int(command)
             print(math.sqrt(command))
+        if line == 'sin ':
+            command = int(command)
+            print(math.sin(math.radians(command)))
+        if line == 'cos ':
+            command = int(command)
+            print(math.cos(math.radians(command)))
+        if line == 'tan ':
+            command = int(command)
+            print(math.tan(math.radians(command)))
+        if line == 'csc ':
+            command = int(command)
+            print(1/(math.sin(math.radians(command))))
+        if line == 'sec ':
+            command = int(command)
+            print(1/(math.cos(math.radians(command))))
+        if line == 'cot ':
+            command = int(command)
+            print(1/(math.tan(math.radians(command))))
+        if line == 'rad ':
+            command = int(command)
+            print(math.radians(command))
         if line == 'clr ':
             print('\n'*50)
         if line == 'hlp ':
@@ -198,7 +257,7 @@ while True:
             print('       - exponents')
             print('       - square roots')
             print('       - factorials')
-            print('  * follow the directions ont eh home page to activate a function')
+            print('  * follow the directions on the home page to activate a function')
     except Exception as e:
         print(e)
         print('sorry, an error occured')
